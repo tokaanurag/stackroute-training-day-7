abf2828980b27dbe51d8d5af7f7a4e2e036f7b7c
anuragitachi <37541905+anuragitachi@users.noreply.github.com>
2021-10-13 10:30:49 +0530

    Exrecise 1
    //Write a program to get the index of all the characters of the alphabet.

---
+++ ex3.java
@@ -0,0 +1,29 @@
+//Write a program to get the index of all the characters of the alphabet.
+package an;
+
+public class ex3 {
+	public static void main(String args[]) {
+	String s1=" the indexing  program";
+	int i1=s1.indexOf('i');
+	int i2 =s1.indexOf('t');
+	int i3=s1.indexOf('n');
+	int i4 =s1.indexOf('d');
+	int i5=s1.indexOf('x');
+	int i6 =s1.indexOf('p');
+	
+	
+	
+System.out.println(i1);
+System.out.println(i2);
+System.out.println(i3);
+System.out.println(i4);
+System.out.println(i5);
+System.out.println(i6);
+
+
+
+
+
+	
+	}
+}
